ea0fb14131bf3b944ef5ff8020aef0aac85b37d1
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-09-12 17:54:34 +0100

    Typography

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+import  Typography  from '@mui/material/Typography';
+
 function App() {
   return (
-    <div>
-      Hello React
-    </div>
+    <Typography variant='h5' component='p' align='center' gutterBottom={true} sx={{backgroundColor:'aliceblue',color:'yellowgreen'}}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt recusandae dolore, quisquam laborum consectetur ad delectus cum rerum vero aspernatur non molestias accusamus doloremque perspiciatis ut possimus numquam sapiente repudiandae.
+    </Typography>
 
   );
 }
