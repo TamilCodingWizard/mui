7309f500c9bd25d1fbc7c740fe8cae356562d017
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-09-13 19:41:24 +0100

    theme

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import { Send } from "@mui/icons-material";
-import { Container } from "@mui/material";
+import { Container, styled } from "@mui/material";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+
+const StyledButton = styled(Button)(({theme}) => ({
+  backgroundColor:'gray',
+        color: theme.palette.primary.main,
+        "&:hover": {
+          backgroundColor:'yellowgreen',
+          color:'gray',
+        }
+}))
 
 function App() {
   return (
     <Container>
-      <Typography
-        variant="h5"
-        component="p"
-        align="center"
-        gutterBottom={true}
-        sx={{ backgroundColor: "aliceblue", color: "yellowgreen" }}
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
-        recusandae dolore, quisquam laborum consectetur ad delectus cum rerum
-        vero aspernatur non molestias accusamus doloremque perspiciatis ut
-        possimus numquam sapiente repudiandae.
-      </Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" onClick={() => console.log('clicked')}>Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+     
       <br/>
-      <Button variant="contained" disabled>Disabled Contained</Button>
-      <Button variant="contained" disableElevation> Contained</Button>
+
+      <StyledButton variant="contained"> My Button</StyledButton>
       <br/>
-      <Button variant="contained" onClick={() => console.log('clicked')} color='error' size='small'>Contained</Button>
-      <br/>
-      <Button variant="contained" sx={{
-        backgroundColor:'gray',
-        color:'yellowgreen',
-        "&:hover": {
-          backgroundColor:'yellowgreen',
-        color:'gray',
-        }
-      }}> My Button</Button>
-      <br/>
-      <br/>
-      <Button variant="contained" endIcon={<Send/>}>Send</Button>
+      <Button variant='contained'>Default Button</Button>
+      
 
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@mui/material';
+import { mytheme } from './Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ThemeProvider theme={mytheme}>
+        <App />
+    </ThemeProvider>
 );
 
 
